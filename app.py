330a0f43524ae556eaf37f929ed97196fcc1e2f1
Nilansh Bansal <bansalnilansh@gmail.com>
2018-07-12 14:02:16 +0530

    get post completed

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-class Student(Resource):
+class Item(Resource):
     def get(self,name):
-        return {'student':name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item':None}, 404
     
-api.add_resource(Student,'/student/<string:name>')
+    def post(self,name):
+        item = {'name':name, 'price':12.00}
+        items.append(item)
+        return item,201  #201 status code for insertion successful (Created)
+        #202 for showing clients that we will create the object but not now so your request is accepted
+
+api.add_resource(Item,'/item/<string:name>')
 
 
 app.run(port=5000)
