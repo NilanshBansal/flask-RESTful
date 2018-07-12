998fa6b03b3930b469a8871554b3326773504c07
Nilansh Bansal <bansalnilansh@gmail.com>
2018-07-12 16:52:10 +0530

    completed

--- app.py
+++ app.py
@@ -1,55 +1,73 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_jwt import JWT,jwt_required
-from security import authenticate,identity
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'jose'
 api = Api(app)
 
-jwt = JWT(app,authenticate,identity) #creates endpoint /auth
+jwt = JWT(app, authenticate, identity)  # creates endpoint /auth
 
 items = []
 
+
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help='This field cannot be left blank!'
+    )
+
     @jwt_required()
-    def get(self,name):
-        item = next(filter(lambda x: x['name'] == name, items),None) #returns the first matched item else None
-        return {'item':item}, 200 if item else 404
-    
-    def post(self,name):
-        if next(filter(lambda x: x['name'] == name, items),None):
-            return {'message':"An item with '{}' already exists.".format(name)},400 #400 for bad request as client made req with wrong name.
-        data = request.get_json()
-        item = {'name':name, 'price':data['price']}
+    def get(self, name):
+        # returns the first matched item else None
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        
+        if next(filter(lambda x: x['name'] == name, items), None):
+            # 400 for bad request as client made req with wrong name.
+            return {'message': "An item with '{}' already exists.".format(name)}, 400
+        
+        data = Item.parser.parse_args()
+
+        item = {'name': name, 'price': data['price']}
         items.append(item)
-        return item,201  #201 status code for insertion successful (Created)
-        #202 for showing clients that we will create the object but not now so your request is accepted
+        return item, 201  # 201 status code for insertion successful (Created)
+        # 202 for showing clients that we will create the object but not now so your request is accepted
 
-    def delete(self,name):
+    def delete(self, name):
         global items
-        if next(filter(lambda x: x['name'] == name, items),None) is None:
-            return {'message':"No item with name '{}' ".format(name)},400
+        if next(filter(lambda x: x['name'] == name, items), None) is None:
+            return {'message': "No item with name '{}' ".format(name)}, 400
 
-        items = list(filter(lambda x: x['name'] != name,items))
-        return {'message':'Item deleted'}
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
 
-    def put(self,name):
-        data = request.get_json()
-        item = next(filter(lambda x: x['name'] == name ,items),None)
+    def put(self, name):
+
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
-            item = {'name':name, 'price':data['price']}
+            item = {'name': name, 'price': data['price']}
             items.append(item)
         else:
             item.update(data)
         return item
 
+
 class ItemList(Resource):
     def get(self):
-        return {'items':items}
+        return {'items': items}
 
-api.add_resource(Item,'/item/<string:name>')
-api.add_resource(ItemList,'/items')
+        
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 
-app.run(port=5000,debug=True)
+app.run(port=5000, debug=True)
